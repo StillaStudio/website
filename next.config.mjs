e1e33f8c398f5ee995ae396a685abf3c40ebd38e
Miguel Matos <miguel.matos@oribi.nl>
2026-02-02 23:18:42 +0000

    fix base path

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 
 const nextConfig = {
   output: 'export',
+  basePath: '/website',
   typescript: {
     ignoreBuildErrors: true,
   },
