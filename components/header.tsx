f17dca575bc56b1c7046c36fac51adba26bd3005
StillaStudio <hello@still-a-studio.com>
2026-02-08 21:47:11 +0000

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -11,7 +11,7 @@ export function Header({dark}: {dark?: boolean}) {
 
   return (
     <header className={`${!dark? "text-white bg-transparent" : 'bg-background'} fixed top-0 left-0 right-0 z-50`}>
-      <div className="container mx-auto px-4 md:px-6 py-3 md:py-4 flex items-center justify-between">
+      <div className="container mx-auto px-6 py-3 md:py-4 flex items-center justify-between">
         <Link
           href="/"
           className="text-base md:text-2xl font-normal relative"
